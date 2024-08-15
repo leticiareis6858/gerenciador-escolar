4904454f69b27a6e4971fe8c9cde826612570497
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-15 00:15:32 -0300

    feat: configura o formulario de recuperacao de senha

--- FormRecuperarSenha/FormRecuperarSenha.Designer.cs
+++ FormRecuperarSenha/FormRecuperarSenha.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_nome = new System.Windows.Forms.TextBox();
             this.btn_mudar = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_nova_senha = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.btn_voltar = new System.Windows.Forms.Button();
@@ -46,16 +46,16 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(115, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.Size = new System.Drawing.Size(133, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nome de usuário";
+            this.label1.Text = "Nome de usuário:";
             // 
-            // textBox1
+            // txt_nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(138, 20);
-            this.textBox1.TabIndex = 1;
+            this.txt_nome.Location = new System.Drawing.Point(119, 74);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(138, 20);
+            this.txt_nome.TabIndex = 1;
             // 
             // btn_mudar
             // 
@@ -66,6 +66,7 @@
             this.btn_mudar.TabIndex = 2;
             this.btn_mudar.Text = "Mudar senha";
             this.btn_mudar.UseVisualStyleBackColor = true;
+            this.btn_mudar.Click += new System.EventHandler(this.btn_mudar_Click);
             // 
             // label2
             // 
@@ -73,9 +74,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(115, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 20);
+            this.label2.Size = new System.Drawing.Size(141, 20);
             this.label2.TabIndex = 3;
-            this.label2.Text = "E-mail cadastrado";
+            this.label2.Text = "E-mail cadastrado:";
             // 
             // label3
             // 
@@ -83,23 +84,23 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(115, 168);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.Size = new System.Drawing.Size(97, 20);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Nova senha";
+            this.label3.Text = "Nova senha:";
             // 
-            // textBox2
+            // txt_email
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 133);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(138, 20);
-            this.textBox2.TabIndex = 5;
+            this.txt_email.Location = new System.Drawing.Point(119, 133);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(138, 20);
+            this.txt_email.TabIndex = 5;
             // 
-            // textBox3
+            // txt_nova_senha
             // 
-            this.textBox3.Location = new System.Drawing.Point(119, 191);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(138, 20);
-            this.textBox3.TabIndex = 6;
+            this.txt_nova_senha.Location = new System.Drawing.Point(119, 191);
+            this.txt_nova_senha.Name = "txt_nova_senha";
+            this.txt_nova_senha.Size = new System.Drawing.Size(138, 20);
+            this.txt_nova_senha.TabIndex = 6;
             // 
             // label4
             // 
@@ -131,6 +132,7 @@
             this.btn_voltar.TabIndex = 9;
             this.btn_voltar.Text = "Voltar";
             this.btn_voltar.UseVisualStyleBackColor = true;
+            this.btn_voltar.Click += new System.EventHandler(this.btn_voltar_Click);
             // 
             // FormRecuperarSenha
             // 
@@ -140,12 +142,12 @@
             this.Controls.Add(this.btn_voltar);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_nova_senha);
+            this.Controls.Add(this.txt_email);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btn_mudar);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_nome);
             this.Controls.Add(this.label1);
             this.Name = "FormRecuperarSenha";
             this.Text = "Tela de recuperação de senha";
@@ -157,12 +159,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_nome;
         private System.Windows.Forms.Button btn_mudar;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_nova_senha;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btn_voltar;
--- FormRecuperarSenha/FormRecuperarSenha.cs
+++ FormRecuperarSenha/FormRecuperarSenha.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GerenciadorEscolar;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,46 @@ namespace GerenciadorEscolar
 {
     public partial class FormRecuperarSenha : Form
     {
+        private Database db;
+
         public FormRecuperarSenha()
         {
             InitializeComponent();
+            db = new Database();
+        }
+
+        private void btn_voltar_Click(object sender, EventArgs e)
+        {
+            form_login formLogin=new form_login();
+
+            this.Hide();
+            formLogin.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_mudar_Click(object sender, EventArgs e)
+        {
+            String nome=txt_nome.Text.Trim();
+            String email=txt_email.Text.Trim();
+            String novaSenha = txt_nova_senha.Text.Trim();
+
+            bool alunoValido = db.verificarNomeEmailAluno(nome, email);
+            bool professorValido = db.verificarNomeEmailProfessor(nome, email);
+
+            if(alunoValido)
+            {
+                bool mudarSenha = db.mudarSenhaAluno(email, novaSenha);
+                MessageBox.Show("Senha alterada com sucesso!");
+                
+            } else if (professorValido)
+            {
+                bool mudarSenha = db.mudarSenhaProfessor(email, novaSenha);
+                MessageBox.Show("Senha alterada com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Nome de usuário e email inválidos!");
+            }
         }
     }
 }

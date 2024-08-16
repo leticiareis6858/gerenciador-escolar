ff4e19cd0e29ed9c561299a689e8ddd6dcba8b68
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-15 21:57:07 -0300

    feat: configura o formulario de atualizar o cadastro de um professor

---
+++ FormsProfessor/FormAtualizarCadastroProfessores.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace GerenciadorDeTurmas.FormsProfessor
+{
+    partial class FormAtualizarCadastroProfessores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_voltar = new System.Windows.Forms.Button();
+            this.txt_titulacao = new System.Windows.Forms.ComboBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.txt_formacao = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.btn_atualizar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.Location = new System.Drawing.Point(441, 167);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(102, 49);
+            this.btn_limpar.TabIndex = 50;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_voltar
+            // 
+            this.btn_voltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_voltar.Location = new System.Drawing.Point(508, 16);
+            this.btn_voltar.Name = "btn_voltar";
+            this.btn_voltar.Size = new System.Drawing.Size(78, 30);
+            this.btn_voltar.TabIndex = 49;
+            this.btn_voltar.Text = "Voltar";
+            this.btn_voltar.UseVisualStyleBackColor = true;
+            this.btn_voltar.Click += new System.EventHandler(this.btn_voltar_Click);
+            // 
+            // txt_titulacao
+            // 
+            this.txt_titulacao.FormattingEnabled = true;
+            this.txt_titulacao.Items.AddRange(new object[] {
+            "Técnico(a)",
+            "Bacharel",
+            "Especialista",
+            "Licenciado(a)",
+            "Mestre(a)",
+            "Doutor(a)",
+            "Pós-Doutor(a)",
+            "Professor(a) Titular",
+            "Professor(a) Associado(a)",
+            "Professor(a) Assistente"});
+            this.txt_titulacao.Location = new System.Drawing.Point(57, 190);
+            this.txt_titulacao.Name = "txt_titulacao";
+            this.txt_titulacao.Size = new System.Drawing.Size(223, 21);
+            this.txt_titulacao.TabIndex = 48;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(315, 136);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(228, 20);
+            this.txt_senha.TabIndex = 47;
+            // 
+            // txt_formacao
+            // 
+            this.txt_formacao.Location = new System.Drawing.Point(57, 136);
+            this.txt_formacao.Name = "txt_formacao";
+            this.txt_formacao.Size = new System.Drawing.Size(223, 20);
+            this.txt_formacao.TabIndex = 46;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(316, 78);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(227, 20);
+            this.txt_email.TabIndex = 45;
+            // 
+            // btn_atualizar
+            // 
+            this.btn_atualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_atualizar.Location = new System.Drawing.Point(316, 167);
+            this.btn_atualizar.Name = "btn_atualizar";
+            this.btn_atualizar.Size = new System.Drawing.Size(102, 49);
+            this.btn_atualizar.TabIndex = 43;
+            this.btn_atualizar.Text = "Atualizar";
+            this.btn_atualizar.UseVisualStyleBackColor = true;
+            this.btn_atualizar.Click += new System.EventHandler(this.btn_atualizar_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(312, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 20);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Senha:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(53, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 20);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Titulação:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Formação:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(312, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "E-mail:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 20);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Para alterar seu nome contate alguém da direção.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(47, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Atenção!!!";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(57, 78);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.ReadOnly = true;
+            this.txt_nome.Size = new System.Drawing.Size(223, 20);
+            this.txt_nome.TabIndex = 53;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 54;
+            this.label5.Text = "Nome:";
+            // 
+            // FormAtualizarCadastroProfessores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 295);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_voltar);
+            this.Controls.Add(this.txt_titulacao);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_formacao);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.btn_atualizar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "FormAtualizarCadastroProfessores";
+            this.Text = "Atualizar Cadastro Professor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_voltar;
+        private System.Windows.Forms.ComboBox txt_titulacao;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.TextBox txt_formacao;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Button btn_atualizar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ FormsProfessor/FormAtualizarCadastroProfessores.cs
@@ -0,0 +1,77 @@
+﻿using GerenciadorEscolar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorDeTurmas.FormsProfessor
+{
+    public partial class FormAtualizarCadastroProfessores : Form
+    {
+        private Database db;
+        public FormAtualizarCadastroProfessores()
+        {
+            InitializeComponent();
+            db = new Database();
+            int id=db.BuscarIdProfessorPorNome(txt_nome.Text);
+            txt_formacao.Text = db.BuscarFormacaoProfessorPorNome(txt_nome.Text);
+            txt_titulacao.Text = db.BuscarTitulacaoProfessorPorNome(txt_nome.Text);
+            txt_email.Text = db.BuscarEmailProfessorPorNome(txt_nome.Text);
+            txt_senha.Text = db.BuscarSenhaProfessorPorNome(txt_nome.Text);
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_nome.Clear();
+            txt_email.Clear();
+            txt_senha.Clear();
+            txt_formacao.Clear();
+            txt_titulacao.SelectedItem = null;
+        }
+
+        private void btn_voltar_Click(object sender, EventArgs e)
+        {
+            FormTelaInicialProfessor formTelaInicial = new FormTelaInicialProfessor();
+            formTelaInicial.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_atualizar_Click(object sender, EventArgs e)
+        {
+            if(txt_email.Text==null||txt_senha.Text==null||txt_formacao==null||txt_titulacao.SelectedItem==null)
+            {
+                MessageBox.Show("Preencha ao menos um dos campos!");
+            } else if(txt_email.Text!=null)
+            {
+                db.AtualizarEmailProfessor(txt_nome.Text, txt_email.Text);
+                MessageBox.Show("Email atualizado com sucesso!");
+            } else if(txt_senha!=null)
+            {
+                db.AtualizarSenhaProfessor(txt_nome.Text, txt_senha.Text);
+                MessageBox.Show("Senha atualizada com sucesso!");
+            } else if(txt_formacao!=null)
+            {
+                db.AtualizarFormacaoProfessor(txt_nome.Text, txt_formacao.Text);
+                MessageBox.Show("Formação atualizada com sucesso!");
+            } else if(txt_titulacao.SelectedItem!=null)
+            {
+                db.AtualizarTitulacaoProfessor(txt_nome.Text, txt_titulacao.SelectedItem.ToString());
+                MessageBox.Show("Titulação atualizada com sucesso!");
+            } else if(txt_email.Text != null || txt_senha.Text != null || txt_formacao != null || txt_titulacao.SelectedItem != null)
+            {
+                db.AtualizarProfessor(txt_nome.Text,txt_email.Text, txt_senha.Text, txt_formacao.Text, txt_titulacao.Text);
+                MessageBox.Show("Cadastro atualizado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao atualizar!");
+            }
+
+        }
+    }
+}

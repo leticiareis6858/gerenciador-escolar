df26771e34e63da7cd07c882fc09814e7ae6da22
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-18 18:45:50 -0300

    feat: cria e configura o formulario para buscar cursos relacionados a um aluno

---
+++ FormsAluno/FormBuscarCursosAlunos.cs
@@ -0,0 +1,75 @@
+﻿using GerenciadorEscolar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorDeTurmas.FormsAluno
+{
+    public partial class FormBuscarCursosAlunos : Form
+    {
+        private Database db;
+        public FormBuscarCursosAlunos()
+        {
+            InitializeComponent();
+            db = new Database();
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            String idAluno = db.BuscarIdAlunoPorNome(txt_nome.Text);
+            if (txt_id_curso!=null)
+            {
+                int idCurso = int.Parse(txt_id_curso.Text);
+                DataTable resultado = db.BuscarCursoDeAlunoPorId(idCurso, idAluno);
+
+                dataGridView_cursos.DataSource = resultado;
+            }
+            else if (txt_curso != null)
+            {
+                String nomeCurso = txt_curso.Text;
+                DataTable resultado = db.BuscarCursoDeAlunoPorNome(nomeCurso, idAluno);
+
+                dataGridView_cursos.DataSource = resultado;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, preencha ao menos um dos campos para buscar por um curso.");
+            }
+        }
+
+        private void btn_voltar_Click(object sender, EventArgs e)
+        {
+            FormTelaInicialAluno telaInicial= new FormTelaInicialAluno();
+            telaInicial.Show();
+            this.Close();
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você realmente deseja sair?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                form_login form_login = new form_login();
+                form_login.Show();
+                this.Close();
+            }
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_id_curso.Text = string.Empty;
+            txt_curso.Text = string.Empty;
+
+            DataTable dt = db.BuscarDisciplinas();
+            dataGridView_cursos.DataSource = dt;
+        }
+
+    }
+}

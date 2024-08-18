a25fd09eff003fe40a88831598a8bdd688583dee
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-18 16:21:30 -0300

    feat: adiciona funcao para buscar cidade de um aluno atraves de seu nome

--- Database.cs
+++ Database.cs
@@ -817,4 +817,19 @@ public class Database
             return result.ToString();
         }
     }
+
+    public String BuscarCidadeAlunoPorNome(String nome)
+    {
+        using (MySqlConnection conn = GetConnection())
+        {
+            conn.Open();
+            string query = "SELECT cidade_aluno FROM tb_aluno WHERE nome_aluno = @nome_aluno";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@nome_aluno", nome);
+
+            object result = cmd.ExecuteScalar();
+
+            return result.ToString();
+        }
+    }
 }

addead82c97d258ff8652c4eb301ba4b260eeb99
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-18 16:21:08 -0300

    feat: adiciona funcao para buscar telefone de um aluno atraves de seu nome

--- Database.cs
+++ Database.cs
@@ -802,4 +802,19 @@ public class Database
             return result.ToString();
         }
     }
+
+    public String BuscarTelefoneAlunoPorNome(String nome)
+    {
+        using (MySqlConnection conn = GetConnection())
+        {
+            conn.Open();
+            string query = "SELECT telefone_aluno FROM tb_aluno WHERE nome_aluno = @nome_aluno";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@nome_aluno", nome);
+
+            object result = cmd.ExecuteScalar();
+
+            return result.ToString();
+        }
+    }
 }

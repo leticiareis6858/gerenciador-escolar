737c2ac0a717ec7fd221d42e1c418f8428d3d0f6
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-08-18 19:17:07 -0300

    feat: adiciona funcao para buscar todas as disciplinas de um aluno

--- Database.cs
+++ Database.cs
@@ -1053,4 +1053,21 @@ public class Database
             return dt;
         }
     }
+
+    public DataTable BuscarDisciplinasAluno(String idAluno)
+    {
+        using (MySqlConnection conn = GetConnection())
+        {
+            conn.Open();
+            string query = @"SELECT tb_disciplina.* FROM tb_disciplina_aluno INNER JOIN tb_disciplina ON tb_disciplina_aluno.id_disciplina = tb_disciplina.id_disciplina WHERE tb_disciplina_aluno.matricula_aluno = @id_aluno";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id_aluno", idAluno);
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+        }
+    }
 }

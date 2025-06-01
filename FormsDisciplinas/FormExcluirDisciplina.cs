fbeb38de197929a488eb47e51b77a86e4fd447bf
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2025-06-01 20:12:07 -0300

    feat: adiciona tela para excluir disciplinas

--- FormsDisciplinas/FormExcluirDisciplina.cs
+++ FormsDisciplinas/FormExcluirDisciplina.cs
@@ -12,9 +12,28 @@ namespace GerenciadorEscolar.FormsDisciplinas
 {
     public partial class FormExcluirDisciplina : Form
     {
+        private Database db;
         public FormExcluirDisciplina()
         {
             InitializeComponent();
+            db = new Database();
+        }
+
+        private void PreencherListBoxDisciplinas()
+        {
+            List<string> disciplinas = db.BuscarNomeIdDisciplinas();
+            list_disciplinas.Items.Clear();
+
+            foreach (string disciplina in disciplinas)
+            {
+                list_disciplinas.Items.Add(disciplina);
+            }
+        }
+
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
